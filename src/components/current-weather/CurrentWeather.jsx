18a28a5b2ad8e7f878ca862574a21cd9b04f4b65
Alper Yazagan <alperyazagan@gmail.com>
2023-04-22 17:58:07 +0300

    building

--- src/components/current-weather/CurrentWeather.jsx
+++ src/components/current-weather/CurrentWeather.jsx
@@ -22,15 +22,15 @@ const CurrentWeather = () => {
             <span className="parameter-value">22°C</span>
           </div>
           <div className="parameter-row">
-            <span className="parameter-label">wind</span>
+            <span className="parameter-label">Wind</span>
             <span className="parameter-value">2 m/s</span>
           </div>
           <div className="parameter-row">
-            <span className="parameter-label">humidity</span>
+            <span className="parameter-label">Humidity</span>
             <span className="parameter-value">15%</span>
           </div>
           <div className="parameter-row">
-            <span className="parameter-label">pressure</span>
+            <span className="parameter-label">Pressure</span>
             <span className="parameter-value">15 hPa</span>
           </div>
         </div>
